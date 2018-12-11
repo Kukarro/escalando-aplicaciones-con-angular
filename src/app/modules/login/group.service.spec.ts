5b8324478f0547a5d84463a97f9e1ed18e7a6bb5
Daniel Moya <danielmoyavidal@gmail.com>
2018-12-11 11:33:01 -0300

    Pruebas unitarias

--- src/app/modules/login/group.service.spec.ts
+++ src/app/modules/login/group.service.spec.ts
@@ -1,4 +1,52 @@
+// librearia para realizar test
 import { TestBed } from '@angular/core/testing';
+// lo que se va a testear
+import { GroupService } from './group.service';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+
+class HttpClientMock  {
+  get = jasmine.createSpy();
+}
+
+describe('GroupService', () => {
+  let service: GroupService;
+  let httpClientMock: HttpClientMock;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [ GroupService,
+        {
+          provide: HttpClient,
+          useClass: HttpClientMock
+        }
+      ]
+    });
+    service = TestBed.get(GroupService);
+    httpClientMock = TestBed.get(HttpClient);
+  });
+
+  it('PRUEBA1: El servicio esta definido.', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('PRUEBA2: deberia llamar al servicio', () => {
+    httpClientMock.get.and.returnValue({
+      toPromise() {}
+    });
+    service.getGroups();
+    expect(httpClientMock.get).toHaveBeenCalledWith(environment.endpoint.groups);
+  });
+});
+
+
+/*import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GroupService } from './group.service';
@@ -18,3 +66,4 @@ describe('GroupService', () => {
     expect(service).toBeTruthy();
   });
 });
+*/
